c6f2b9ec6d5729f47b607d734d5fe21d988fe320
John Oni <onijohn1025@gmail.com>
2020-07-18 22:17:26 -0400

    update

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+#    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def john(request):
     return HttpResponse("Hello, John!")
@@ -12,4 +13,7 @@ def david(request):
     return HttpResponse("Hello, David")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+#    return HttpResponse(f"Hello, {name.capitalize()}!")
+     return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+     })
