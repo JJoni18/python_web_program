c9e8aa1d45c84784a444404a68302fb23ec0e336
John Oni <onijohn1025@gmail.com>
2020-07-18 22:04:19 -0400

    update

--- hello/urls.py
+++ hello/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index),
+    path("", views.index, name="index"),
     path("<str:name>", views.greet, name="greet"),
     path("john", views.john, name="john"),
     path("david", views.david, name="David")
